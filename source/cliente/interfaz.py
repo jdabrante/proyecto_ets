23e980512378cb2ec10430a9b8490a1fb18487cc
Rau080 <raulrodrigue87@gmail.com>
2023-04-21 15:29:02 +0100

    Realización del programacion python #25

--- source/cliente/interfaz.py
+++ source/cliente/interfaz.py
@@ -2,19 +2,26 @@ from datetime import datetime
 
 
 class Round:
-    pass
-
+    def __init__(self, id: int):
+        self.id = id
 
 class Game:
-    def __init__(self):
-        self.date = datetime.now()
+    def __init__(self, name: str):
+        self.start_datetime = datetime.now()
+        self.name = name
+        self.rounds = []
 
-    pass
+    def calc_duration(self):
+        self.duration = datetime.now() - self.date
 
+    def add_round(self, round):
+        self.rounds.append(round)
 
 class Map:
     pass
 
 
-test_object = Game()
-print(test_object.date)
+test_object = Game('hola')
+test_round = Round(1)
+test_object.add_round(test_round)
+print(test_object.rounds)
