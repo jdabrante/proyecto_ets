7cb86924d07dc04b2094231fe8c15aa9f780494c
Javier-Garcia-Hernandez <javigh1903@movistar.es>
2023-06-06 14:28:42 +0000

    testing completo, calidad dudosa, 100

--- source/cliente/interfaz.py
+++ source/cliente/interfaz.py
@@ -2,8 +2,10 @@ import filecmp
 from pathlib import Path
 from datetime import datetime
 
+POSSIBLE_OUTCOMES = ("win", "lose", "split")
 
-db_path = "/home/dimas/Github/proyecto_ets/source/cliente/data/odds.dat"
+# db_path = "/home/dimas/Github/proyecto_ets/source/cliente/data/odds.dat"
+db_path = "/home/usuario/GitHub/proyecto_ets/source/cliente/data/odds.dat"
 
 
 class Round:
@@ -12,6 +14,10 @@ class Round:
         self.outcome = "unknown"
 
     def update_outcome(self, outcome: str):
+        if outcome not in POSSIBLE_OUTCOMES:
+            raise UnexpectedOutcomeError(
+                "Unexpected outcome, failed to update round data."
+            )
         self.outcome = outcome
 
     def generate_number(self, current_game):
@@ -42,7 +48,8 @@ class Game:
         return hands_odds
 
     def calc_duration(self):
-        self.duration = datetime.now() - self.start_datetime
+        self.end_datetime = datetime.now()
+        self.duration = self.end_datetime - self.start_datetime
 
     def add_round(self, round):
         round.generate_number(self)
@@ -61,8 +68,12 @@ class Map:
         return f"{self.odds}"
 
 
-my_game = Game("My game")
-current_round = Round("AHAS")
-my_game.add_round(current_round)
-loaded_map = Map(current_round, my_game)
-print(loaded_map)
+class UnexpectedOutcomeError(Exception):
+    pass
+
+
+# my_game = Game("My game")
+# current_round = Round("AHAS")
+# my_game.add_round(current_round)
+# loaded_map = Map(current_round, my_game)
+# print(loaded_map)
--- source/cliente/test_interfaz.py
+++ source/cliente/test_interfaz.py
@@ -1,11 +1,13 @@
 import interfaz
 import pytest
 
-r = interfaz.Round('AHAS')
-g = interfaz.Game('Javier')
+r = interfaz.Round("AHAS")
+g = interfaz.Game("Javier")
+m = interfaz.Map(r, g)
+
 
 def test_build_round():
-    assert r.hand == 'AHAS'
+    assert r.hand == "AHAS"
     assert r.outcome == "unknown"
 
 
@@ -13,3 +15,33 @@ def test_build_game():
     assert g.name == "Javier"
     assert g.rounds == []
     assert g.next_available_round_id == 0
+
+
+def test_add_round():
+    id = g.next_available_round_id
+    g.add_round(r)
+    assert g.rounds[-1] == r
+    assert r.number == id
+    assert g.next_available_round_id == id + 1
+
+
+def test_build_map():
+    m.odds == g.hands_odds[r.hand]
+    str(m) == str(g.hands_odds[r.hand])
+
+
+def test_calc_duration():
+    start_time = g.start_datetime
+    g.calc_duration()
+    end_time = g.end_datetime
+    assert g.duration == end_time - start_time
+
+
+def test_update_outcome():
+    r.update_outcome("win")
+    assert r.outcome == "win"
+    r.update_outcome("lose")
+    assert r.outcome == "lose"
+    with pytest.raises(interfaz.UnexpectedOutcomeError) as err:
+        r.update_outcome("parchita")
+    assert str(err.value) == "Unexpected outcome, failed to update round data."
